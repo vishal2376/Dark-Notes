5de98acd34477d00d5b62007cd9047320d356dd9
vishal2376 <vishalsingh2376@gmail.com>
2022-08-23 15:48:46 +0530

    new note activity layout created

---
+++ app/src/main/java/com/vishal/darknotes/NewNoteActivity.kt
@@ -0,0 +1,14 @@
+package com.vishal.darknotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+
+
+
+    }
+}

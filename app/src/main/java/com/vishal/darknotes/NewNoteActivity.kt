074b5feccbdf9a60fa2d44a5181a401125778c60
vishal2376 <vishalsingh2376@gmail.com>
2022-08-23 16:07:10 +0530

    implement fabSave button

--- app/src/main/java/com/vishal/darknotes/NewNoteActivity.kt
+++ app/src/main/java/com/vishal/darknotes/NewNoteActivity.kt
@@ -2,13 +2,23 @@ package com.vishal.darknotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_note.*
 
 class NewNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_note)
 
+        fabSave.setOnClickListener {
 
+            var title = etTitle.text.toString()
+            var desc = etDescription.text.toString()
+
+            if title.isNotEmpty() and desc.isNotEmpty(){
+
+                saveNote()
+            }
+        }
 
     }
 }

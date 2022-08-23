fb14923c0dad730d3b89a6ed4300a22425f946ad
vishal2376 <vishalsingh2376@gmail.com>
2022-08-23 22:52:59 +0530

    view note layout done

---
+++ app/src/main/java/com/vishal/darknotes/ViewNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.vishal.darknotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_note)
+    }
+}
